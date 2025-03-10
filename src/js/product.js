3e883268c31ab3f4a5d7d046c30e57586beac8bb
brotherblazzard <blazzardj@byui.edu>
2025-03-10 08:57:55 -0600

    package update

--- src/js/product.js
+++ src/js/product.js
@@ -5,7 +5,7 @@ const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
-  cartItems.push(product); 
+  cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
 }
 
